9909f23489b8fd57425b31ab3208a6eb1b078724
hzlizx <978744976@qq.com>
2019-03-25 15:49:03 +0800

    fix docs

--- 0209-minimum-size-subarray-sum/MinimumSizeSubarraySum.java
+++ 0209-minimum-size-subarray-sum/MinimumSizeSubarraySum.java
@@ -1,12 +1,32 @@
 /**
  * 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的连续子数组。如果不存在符合条件的连续子数组，返回 0。
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 输入: s = 7, nums = [2,3,1,2,4,3]
  * 输出: 2
  * 解释: 子数组 [4,3] 是该条件下的长度最小的连续子数组。
  */
 public class MinimumSizeSubarraySum {
+    //todo 还没完成
+    public int minSubArrayLen(int s, int[] nums) {
+        int p1 = 0, p2 = 0, count = 0, sum = 0, diff = 0;
+        int length = nums.length;
+        for (p1 = 0; p1 < length; p1++) {
+            sum = 0;
+            sum += nums[p1];
+            count++;
+            if (sum > s) {
+                return 1;
+            }
+            int factor = nums[p1 + 1] - nums[p1];
+            for (p2 = p1 + 1; p2 < length; p2++) {
+                diff = nums[p2] - nums[p2 - 1];
+                if (diff == factor) {
 
+                }
+            }
+        }
+        return 0;
+    }
 }
