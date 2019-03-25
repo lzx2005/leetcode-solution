bc199ae2bcf2c9360a673d10b0a42c480c8de6a4
hzlizx <978744976@qq.com>
2019-03-25 17:35:31 +0800

    fix 209

--- 0209-minimum-size-subarray-sum/MinimumSizeSubarraySum.java
+++ 0209-minimum-size-subarray-sum/MinimumSizeSubarraySum.java
@@ -8,25 +8,68 @@
  * 解释: 子数组 [4,3] 是该条件下的长度最小的连续子数组。
  */
 public class MinimumSizeSubarraySum {
-    //todo 还没完成
+
+    //todo 还没做完
+
+    public static void main(String[] args) {
+        int[] arr = {12,28,83,4,25,26,25,2,25,25,25,12};
+        int i = new MinimumSizeSubarraySum().minSubArrayLen(213, arr);
+        System.out.println(i);
+    }
+
+
     public int minSubArrayLen(int s, int[] nums) {
-        int p1 = 0, p2 = 0, count = 0, sum = 0, diff = 0;
-        int length = nums.length;
-        for (p1 = 0; p1 < length; p1++) {
-            sum = 0;
-            sum += nums[p1];
-            count++;
-            if (sum > s) {
+        int p1 = 0, p2 = 0;
+        for (int num : nums) {
+            if (num >= s) {
                 return 1;
             }
-            int factor = nums[p1 + 1] - nums[p1];
-            for (p2 = p1 + 1; p2 < length; p2++) {
-                diff = nums[p2] - nums[p2 - 1];
-                if (diff == factor) {
-
+        }
+        while (p1 < nums.length && p2 < nums.length) {
+            if (isSorted(nums, p1, p2)) {
+                if (sum(nums, p1, p2) >= s) {
+                    int pp1 = p1;
+                    while (pp1 <= p2) {
+                        if (sum(nums, pp1, p2) >= s) {
+                            pp1 += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                    pp1 -= 1;
+                    return (p2 - pp1) + 1;
+                } else {
+                    p2++;
                 }
+            } else {
+                p1++;
             }
         }
         return 0;
+    }
+
+    private boolean isSorted(int[] array, int begin, int end) {
+        int length = end - begin;
+        if (length == 0 || length == 1) {
+            return true;
+        }
+        int diff = array[begin + 1] - array[begin]; // every array[p2] - array[p1] should equals diff
+        if (diff != 1 && diff != -1) {
+            return false;
+        }
+        for (int i = begin; i < end; i++) {
+            if (array[i + 1] - array[i] != diff) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int sum(int[] array, int begin, int end) {
+        int sum = 0;
+        for (int i = begin; i <= end; i++) {
+            sum += array[i];
+        }
+        return sum;
     }
 }
