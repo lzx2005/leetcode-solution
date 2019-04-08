90f9829ec90f9ec8559a8cd4c8c954a3d5ba6b39
hzlizx <978744976@qq.com>
2019-04-08 10:32:20 +0800

    Interview of Microsoft solutions [array and strings] 200

---
+++ interview-of-microsoft/java/ArrayAndStrings200.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+/**
+ * https://leetcode.com/explore/interview/card/microsoft/30/array-and-strings/213/
+ * Created by hzlizx on 2019/4/2
+ */
+public class ArrayAndStrings200 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> hash = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String s = String.valueOf(chars);
+            hash.computeIfAbsent(s, key -> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(hash.values());
+    }
+}
